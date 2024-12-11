f06733135e29b60519204ae7aa091362c75cfffb
Rodrigo Balbino <rodrigo080599@hotmail.com>
2024-12-11 11:23:09 +0100

    Fix presentation overlapping

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
@@ -20,6 +22,8 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
     private val retrofit = Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
         .baseUrl(SERVER_BASE_URL).build()
     private val oeuvreService = retrofit.create(OeuvreService::class.java)
+
+    private var texte: TextView? = null
 
     override fun onOeuvreCreated(oeuvre: Oeuvre) {
         // I don't know what it does write on top of
@@ -38,7 +42,9 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_fragment)
+        //setContentView(R.layout.main_fragment_presentation)
+        displayMainFragment()
+        //texte = findViewById(R.id.a_text_main) // Initialize TextView
     }
 
     private fun displayAllData(){
@@ -47,12 +53,10 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
             override fun onResponse(call: Call<List<Oeuvre>>, response: Response<List<Oeuvre>>) {
                 // Get the list of all oeuvres
                 val allOeuvres: List<Oeuvre>? = response.body()
-
                 // Add valid oeuvres to CompositionOeuvres
                 if (allOeuvres != null) {
                     allOeuvres.forEach { compositionOeuvres.addOeuvre(it) }
                 }
-
                 // Display the list of oeuvres
                 displayOeuvreListFragment()
             }
@@ -69,19 +73,21 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
     private fun displayOeuvreListFragment() {
         //declares the fragment transaction
         val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+
         //declares new fragment listfragçentLessDetail
         val oeuvreListFragmentLessDetail = OeuvreListFragmentLessDetail.newInstance(
             compositionOeuvres.getAllOeuvres(),
         )
         //replaces the actual fragment with the one created
-        fragmentTransaction.replace(R.id.a_info_lyt_container_oeuvre, oeuvreListFragmentLessDetail)
+        fragmentTransaction.replace(R.id.a_main_fragment_container, oeuvreListFragmentLessDetail)
         fragmentTransaction.commit()
     }
 
     private fun displayInfo(){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val oeuvreinfoFragment = InfoFragment()
-        fragmentTransaction.replace(R.id.a_info_lyt_container_oeuvre, oeuvreinfoFragment)
+        fragmentTransaction.replace(R.id.a_main_fragment_container, oeuvreinfoFragment)
         fragmentTransaction.commit()
     }
 
@@ -90,14 +96,40 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
         val mapsFragment = MapsFragment.newInstance(
             compositionOeuvres.getAllOeuvres(),
             )
-        fragmentTransaction.replace(R.id.a_info_lyt_container_oeuvre, mapsFragment)
+        fragmentTransaction.replace(R.id.a_main_fragment_container, mapsFragment)
         fragmentTransaction.commit()
+    }
+
+    private fun displayMainFragment() {
+        setContentView(R.layout.main_fragment)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val mainFragment = Main_Fragment_presentation() // Initialize MainFragment
+         fragmentTransaction.replace(R.id.a_main_fragment_container, mainFragment)
+        fragmentTransaction.commit() // Commit the transaction
+    }
+
+    private fun updateData(){
+/*
+        compositionOeuvres.clear()
+        displayOeuvreListFragment()
+
+        oeuvreService.createOeuvre(oeuvre)
+            .enqueue {
+                onResponse = {
+                    val oeuvreFromServer: Oeuvre? = it.body()
+                    compositionOeuvres.addOeuvre(oeuvreFromServer!!)
+                    displayOeuvreListFragment()
+                }
+                onFailure = {
+                    Toast.makeText(this@MainActivity, it?.message, Toast.LENGTH_SHORT).show()
+                }
+            }*/
     }
 
     private fun displayCreateOeuvreFragment() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val createOeuvreFragment = CreateOeuvreFragment()
-        fragmentTransaction.replace(R.id.a_info_lyt_container_oeuvre, createOeuvreFragment)
+        fragmentTransaction.replace(R.id.a_main_fragment_container, createOeuvreFragment)
         fragmentTransaction.commit()
     }
 
@@ -108,6 +140,10 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            R.id.action_main -> {
+                displayMainFragment()
+                true
+            }
             R.id.action_delete -> {
                 compositionOeuvres.clear()
                 displayOeuvreListFragment()
@@ -118,18 +154,22 @@ class MainActivity : AppCompatActivity(), OeuvreCreator {
                 true
             }
             R.id.action_Data -> {
+                texte?.visibility = View.INVISIBLE // Hide the TextView
                 displayAllData()
                 true
             }
             R.id.action_update -> {
-                //updateData()
+                //updateData(Oeuvre)
                 true
             }
             R.id.action_maps -> {
+                texte?.visibility = View.INVISIBLE // Hide the TextView
+
                 displayMapsFragment()
                 true
             }
             R.id.action_info -> {
+                texte?.visibility = View.INVISIBLE // Hide the TextView
                 displayInfo()
                 true
             }
---
+++ app/src/main/java/com/ismin/android/Main_Fragment_presentation.kt
@@ -0,0 +1,39 @@
+package com.ismin.android
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class Main_Fragment_presentation : Fragment() {
+
+    private var texte: TextView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val rootView = inflater.inflate(R.layout.main_fragment_presentation,container,false)
+
+        //texte = rootView.findViewById(R.id.welcome_texte)
+
+        return rootView
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Clear the text in the TextView before the view is destroyed
+        texte?.text = ""
+        texte = null // Prevent memory leaks
+    // Cleanup resources or listeners if any
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        // Additional cleanup if necessary
+    }
+
+}
