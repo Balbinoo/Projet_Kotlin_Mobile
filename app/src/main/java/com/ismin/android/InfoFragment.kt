79b6129e70a568067795b8e65683483097c1dd76
Rodrigo Balbino <rodrigo080599@hotmail.com>
2024-12-11 09:45:39 +0100

    Add info Fragment

---
+++ app/src/main/java/com/ismin/android/InfoFragment.kt
@@ -0,0 +1,21 @@
+package com.ismin.android
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val rootView = inflater.inflate(R.layout.info_fragment,container,false)
+
+        return rootView
+    }
+
+}
